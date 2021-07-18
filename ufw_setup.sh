58fb030cc3e55da03f6566db34aa58865efa8d3c
dumbo25 <dumbo25@users.noreply.github.com>
2021-07-17 19:01:56 -0500

    Update ufw_setup.sh

--- ufw_setup.sh
+++ ufw_setup.sh
@@ -15,22 +15,24 @@ echo "set up uncomplicated firwall (ufw)"
 # sudo ufw enable
 
 # configure ufw
-#  ssh uses port 22
-sudo ufw limit ssh
-sudo ufw allow from 192.168.1.0/24 to any port 22
-sudo ufw deny ssh
-
 # deny all incoming requests and allow all outgoing requests
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
+
+#  ssh uses port 22
+sudo ufw limit ssh
+sudo ufw allow from 192.168.1.0/24 to any port 22
+# the following line disables ssh
+# sudo ufw deny ssh
 
 # http uses port and should be allowed within the LAN
 sudo ufw allow from 192.168.1.0/24 to any port 80
 sudo ufw deny http
 
-# https uses port 443 and should be allowed from within th eLAN
+# https uses port 443 and should be allowed from within the LAN
 sudo ufw allow from 192.168.1.0/24 to any port 443
 sudo ufw deny https
 
 # ICMP/ping doesn't use a port. However, this is required to allow pings form within the LAN
 sudo ufw allow from 192.168.1.0/24 to any port 7
+
