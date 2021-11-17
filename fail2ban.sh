455aa3dea48e0c2d2bd642171e5e5f47cab4f149
dumbo25 <dumbo25@users.noreply.github.com>
2021-11-16 22:39:06 -0600

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,1 +1,28 @@
-dfsd
+#!/bin/bash
+# sript to setup fail2ban
+
+Bold=$(tput bold)
+Normal=$(tput sgr0)
+Red=$(tput setaf 1)
+Green=$(tput setaf 2)
+Blue=$(tput setaf 4)
+Black=$(tput sgr0)
+
+echo -e "\n ${Bold}${Blue} Set up fail2ban ${Black}${Normal}"
+
+apt install fail2ban -y
+
+systemctl start fail2ban
+systemctl enable fail2ban
+
+> /etc/fail2ban/jail.local
+echo "[sshd]" >> /etc/fail2ban/jail.local
+echo "enabled = true" >> /etc/fail2ban/jail.local
+echo "port = 22" >> /etc/fail2ban/jail.local
+echo "filter = sshd" >> /etc/fail2ban/jail.local
+echo "logpath = /var/log/auth.log" >> /etc/fail2ban/jail.local
+echo "maxretry = 3" >> /etc/fail2ban/jail.local
+
+systemctl restart fail2ban
+
+echo -e "\n ${Bold}${Blue} fail2ban set up complete ${Black}${Normal}"
