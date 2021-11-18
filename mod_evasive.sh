3f85fa04dac770433d635f275dbfaa8f3f8e4805
dumbo25 <dumbo25@users.noreply.github.com>
2021-11-17 19:33:44 -0600

    Update mod_evasive.sh

--- mod_evasive.sh
+++ mod_evasive.sh
@@ -26,7 +26,8 @@ rm evasive.conf
 wget "https://raw.githubusercontent.com/dumbo25/unsed_rpi/main/evasive.conf"
 
 echo "DEBUG: neeed to change edit to sed"
-sed -i 's/#DOSEmailNotify.*/DOSEmailNotify $YourEmail/g' evasive.conf
+a="s/#DOSEmailNotify.*/DOSEmailNotify $YourEmail/g"
+sed -i '$a' evasive.conf
 exit
 echo "DEBUG: neeed to change above"
 
