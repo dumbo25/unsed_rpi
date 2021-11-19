8afc67a82816da0a7b5461987a61cfef080ad330
dumbo25 <dumbo25@users.noreply.github.com>
2021-11-18 20:52:16 -0600

    Update rpi_monitor.sh

--- rpi_monitor.sh
+++ rpi_monitor.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# script to install and setup mod_evasive
+# script to install and setup rpi_monitor
 #
-# mod_evasive is a module for Apache, which provides evasive action in the event
-# of a Denial of Service attack or brute force attack. Install mod_evasive by 
-# running the command:
+# RPi-Monitor is designed to start automatically and collect metrics. The web 
+# interface is available on address http://raspberrypi.local:8888.
 
 Bold=$(tput bold)
 Normal=$(tput sgr0)
@@ -12,15 +11,15 @@ Green=$(tput setaf 2)
 Blue=$(tput setaf 4)
 Black=$(tput sgr0)
 
-echo -e "\n ${Bold}${Blue} Install and configure mod_evasive to lessen DOS attacks ${Black}${Normal}"
+echo -e "\n ${Bold}${Blue} Install and configure rpi_monitor ${Black}${Normal}"
 
-sudo apt-get install dirmngr
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
-sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
+apt install dirmngr -y
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
+wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
 
-sudo apt-get update
-sudo apt-get install rpimonitor
+apt update
+apt install rpimonitor -y
 
-RPi-Monitor is designed to start automatically and collect metrics. The web interface is available on address http://raspberrypi.local:8888.
+/etc/init.d/rpimonitor update
 
-sudo /etc/init.d/rpimonitor update
+echo -e "\n ${Bold}${Blue} rpi_monitor is done ${Black}${Normal}"
